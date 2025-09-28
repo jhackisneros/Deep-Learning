87980589ae1f348546578b5c1220c21b2861db06
zair <oliveracisneros1@gmail.com>
2025-09-28 10:12:15 +0200

    cambios

--- app/app.py
+++ app/app.py
@@ -16,7 +16,7 @@ app.config['UPLOAD_FOLDER'] = os.path.join('app','uploads')
 os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
 
 # Try to load the model (if exists)
-MODEL_PATH = os.path.join('models','mnist_compiled_model.h5')
+MODEL_PATH = os.path.join('models','mnist_compiled_model.keras')
 model = None
 if os.path.exists(MODEL_PATH):
     model = tf.keras.models.load_model(MODEL_PATH)
